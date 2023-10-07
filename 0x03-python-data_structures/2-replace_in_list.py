62b7402e6fa45cd816dd117413a831d8b61114e7
sherif mostafa <sherifmostafa765@gmail.com>
2023-10-07 16:48:56 -0700

    s

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def replace_in_list(my_list, idx, element):
-        if 0 <= idx < len(my_list):
-            my_list[idx] = element
-        return my_list
+
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list
