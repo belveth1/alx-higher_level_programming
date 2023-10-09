ae5ee55302e8add4e8d52f93a49e77f78141be3c
sherif mostafa <sherifmostafa765@gmail.com>
2023-10-09 13:17:30 -0700

    s

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,8 +2,6 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
 
 /**
  * struct listint_s - singly linked list
@@ -18,27 +16,8 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-/**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
-{
-        listint_t *node = *head, *next, *prev = NULL;
-
-        while (node)
-        {
-                next = node->next;
-                node->next = prev;
-                prev = node;
-                node = next;
-        }
-
-        *head = prev;
-        return (*head);
-}
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
