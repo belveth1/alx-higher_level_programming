6e296a9f0c38070bc0ceeea471fa9646d72124aa
sherif mostafa <sherifmostafa765@gmail.com>
2024-01-09 03:37:57 -0800

    s

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,4 @@
-t line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	  console.log(line);
+#!/usr/bin/node
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
